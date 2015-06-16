9b9c8de11fb133b846fa9869b324a658b2f7765b
asouza <alots.ssa@gmail.com>
2015-06-15 21:34:20 -0300

    ejs e variaveis na pagina. preciso aprender tudo, so copiei

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('pages/index', {
+        nome2: "alberto luiz"
+    });
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
