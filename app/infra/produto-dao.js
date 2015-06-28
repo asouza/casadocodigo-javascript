6a4d6489506e9a53764d8d5c7bc2ad24d339cea5
asouza <alots.ssa@gmail.com>
2015-06-28 16:48:57 -0300

    usando o prototype para definir os metodos das classes

--- app/infra/produto-dao.js
+++ app/infra/produto-dao.js
@@ -1,17 +1,18 @@
+function ProdutoDao(connection) {
+    this._connection = connection;
+}
+
+ProdutoDao.prototype.salva = function(livro,callback) {
+    this.connection.query('INSERT INTO livros SET ?', livro, callback);
+}
+
+ProdutoDao.prototype.lista = lista = function(callback) {
+    connection.query('select * from livros',callback);
+}
+
+ProdutoDao.prototype.buscaPorId = function (id,callback) {
+    connection.query("select * from livros where id = ?",[id],callback);
+}
 module.exports = function(app) {
-
-    app.livroDao = function(connection) {
-        this.salva = function(livro,callback) {
-            connection.query('INSERT INTO livros SET ?', livro, callback);
-        }
-
-        this.lista = function(callback) {
-            connection.query('select * from livros',callback);
-        }
-
-        this.buscaPorId = function (id,callback) {
-            connection.query("select * from livros where id = ?",[id],callback);
-        }
-        return this;
-    }
+    return ProdutoDao;
 }
