f5b5403734a0181ecca917cd8a774f17eff76ca0
asouza <alots.ssa@gmail.com>
2015-07-14 16:34:41 -0300

    testando a listagem de produtos

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require("assert")
 var request = require('supertest');
 
-describe('Array', function() {
+describe('#ProdutosController', function() {
 
     before(function() {
         console.log("rodando algo antes de tudo");
@@ -12,18 +12,17 @@ describe('Array', function() {
     });
 
 
-    describe('#indexOf()', function () {
-        it('testando listagem', function (done) {
-            request("http://localhost:3000")
-                .get('/produtos')
-                .expect(200)
-                .end(function(err,res){
-                    if (err) throw err;
-                    console.log(res.text);
-                    done();
-                });
 
-        });
+    it('#listagem de produtos', function (done) {
+        request("http://localhost:3000")
+            .get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200,done)
+
     });
+
+
+
 
 });
