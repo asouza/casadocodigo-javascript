d1db8e7735e484149ca491bc35232fef4115bcff
asouza <alots.ssa@gmail.com>
2015-07-14 16:47:12 -0300

    adicionando testes para o cadastro de produtos

--- test/test.js
+++ test/test.js
@@ -13,12 +13,36 @@ describe('#ProdutosController', function() {
 
 
 
-    it('#listagem de produtos', function (done) {
+    it('#listagem de produtos json', function (done) {
         request("http://localhost:3000")
             .get('/produtos')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200,done)
+
+    });
+
+    it('#listagem de produtos html', function (done) {
+        request("http://localhost:3000")
+            .get('/produtos')
+            .expect('Content-Type', /html/)
+            .expect(200,done)
+
+    });
+
+    it('#cadastro de um novo produto com tudo preenchido', function (done) {
+        request("http://localhost:3000")
+            .post('/produtos')
+            .send({titulo:"novo livro",preco:20.50,descricao:"livro de teste"})
+            .expect(302,done)
+
+    });
+
+    it('#cadastro de um novo produto com dados invalidos', function (done) {
+        request("http://localhost:3000")
+            .post('/produtos')
+            .send({titulo:"",descricao:"livro de teste"})
+            .expect(400,done)
 
     });
 
