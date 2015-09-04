d006e659c64e642d67058ef668e9f0b4c24daa93
asouza <alots.ssa@gmail.com>
2015-09-04 17:07:16 -0300

    tinha sobrado uma pasta estranha

--- app.routes/promocoes.js
+++
@@ -1,6 +0,0 @@
-var controller = require('../controllers/promocoes')();
-module.exports = function(app) {
-    app.get("/promocoes/form", controller.form);
-    app.post("/promocoes", controller.salva);
-    
-}
--- app/routes/promocoes.js
+++ app/routes/promocoes.js
@@ -1,6 +1,6 @@
+var controller = require('../controllers/promocoes')();
 module.exports = function(app) {
-    var controller = app.controllers.promocoes;
-    app.get("/promocoes/nova", controller.form);
+    app.get("/promocoes/form", controller.form);
     app.post("/promocoes", controller.salva);
     
 }
