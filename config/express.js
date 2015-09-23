b1d17361a44adada3176c7cb9ec036f6c0953c22
asouza <alots.ssa@gmail.com>
2015-09-23 18:38:51 -0300

    configuracao erro 500

--- config/express.js
+++ config/express.js
@@ -6,10 +6,6 @@ var expressValidator = require('express-validator');
 module.exports = function() {
 
     var app = express();
-
-
-
-
     app.use(express.static('./public'));
     app.set('view engine', 'ejs');
     app.set('views','./app/views');
@@ -22,14 +18,17 @@ module.exports = function() {
         .then('infra')
         .into(app);
 
-    //tem que colocar na ordem, caso contrário ele passa pelo middleware e ainda não vai ter acontecido nenhum erro.
-    //app.use(function(error,req, res, next){
-    //    res.status(500).render("erros/500");
-    //});
-
     app.use(function(req, res, next){
         res.status(404).render("erros/404");
     });
+
+    app.use(function(error,req, res, next){
+        res.status(500).render("erros/500");
+    });
+    //tem que colocar na ordem, caso contrário ele passa pelo middleware e ainda não vai ter acontecido nenhum erro.
+
+
+
 
     return app;
 };
